f29f0e9f55118ef67558b0b187256cc34a65707b
mubasharahkafeel <mubasharahkafeel11@gmail.com>
2026-07-23 18:05:41 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const supabaseUrl = 'https://cyhqpebyneqyejenydfu.supabase.co'
-const supabaseKey = 'sb_publishable_rIVNNigOFeACu_FiZd03yg_-48gdzCg'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <div style={{ padding: "40px", fontSize: "30px", color: "black" }}>
+    TEST WORKING
+  </div>
+);
